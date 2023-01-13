6585669412cf959588f0961ccf9efcfd69f0b399
yalthaus <yves.althaus42@gmail.com>
2023-01-13 11:04:33 +0100

    fix: split need remove  semi-colon

--- project/src/parse.cpp
+++ project/src/parse.cpp
@@ -33,24 +33,39 @@ std::string getVal(std::string line, std::string attrib, size_t pos) {
     return (temp);
 }
 
+bool    skip_space(std::string &line)
+{
+    if (line[0] == ' ') {
+        line.erase(0, line.find_first_not_of(' '));
+        return (true);
+    }
+    return (false);
+}
+
+std::string cut_word(std::string &line, size_t pos)
+{
+    std::string temp;
+
+    temp = line.substr(0, pos);
+    line.erase(0, temp.size());
+//    if (temp.back() == ';')
+//        temp.erase(temp.back());
+    return (temp);
+}
+
 std::vector<std::string> split(std::string line, std::string sep)
 {
     std::vector<std::string> ret;
-    std::string temp;
     size_t pos = 0;
 
     while ((pos = line.find(sep)) != std::string::npos)
     {
-        if (line[0] == sep[0]) {
-            line.erase(0, sep.size());
+        if (skip_space(line))
             continue ;
-        }
-        temp = line.substr(0, pos);
-        if (temp.back() == ';')
-            temp.erase(temp.back());
-        ret.push_back(temp);
-        line.erase(0, temp.size());
+        ret.push_back(cut_word(line, pos));
     }
+    if (!line.empty())
+        ret.push_back(cut_word(line, line.back()));
     ret.erase(ret.begin());
     return (ret);
 }
